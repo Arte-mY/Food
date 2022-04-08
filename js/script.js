ddb6e781e4ae6fbc45d7adeb8fecf141e53d1867
artsiom <artsiomyakusik@icloud.com>
2022-04-08 22:10:10 +0200

    lesson56

--- js/script.js
+++ js/script.js
@@ -240,10 +240,8 @@ function showModalByScroll () {
                 `;
                 form.insertAdjacentElement('afterend', statusMessage);
                 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
+                
 
-                request.setRequestHeader('Content-tupe', 'application/json');
                 const formData = new FormData(form);
 
                 const object = {};
@@ -251,19 +249,22 @@ function showModalByScroll () {
                     object[key] = value;
                 });
 
-                const json = JSON.stringify(object);
-
-                request.send(json);
-
-                request.addEventListener('load', () => {
-                    if (request.status === 200) {
-                        console.log(request.response);
-                        showThanksModal(message.success);
-                        form.reset();
-                        statusMessage.remove();
-                    } else {
-                        showThanksModal(message.failure);
-                    }
+                fetch('server.php', {
+                    method: "POST",
+                    headers: {
+                        'Content-tupe': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
                 });
             });
         }
