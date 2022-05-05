e33bbbd76ad38a59b62069437346bf378287621c
Artsiom-Yakusik <78408068+Artsiom-Yakusik@users.noreply.github.com>
2022-05-05 21:39:44 +0200

    Regular Expressions

--- js/script.js
+++ js/script.js
@@ -406,12 +406,15 @@ function showModalByScroll () {
         dots.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
 
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { 
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) { 
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -434,9 +437,9 @@ function showModalByScroll () {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -462,7 +465,7 @@ function showModalByScroll () {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset =  +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset =  deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
